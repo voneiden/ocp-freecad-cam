c89ea817d9caeb1c630c72ee38a1d07fd62ded39
Matti Eiden <snaipperi@gmail.com>
2023-06-18 22:52:26 +0300

    Remove dead code

--- src/ocp_freecad_cam/api.py
+++ src/ocp_freecad_cam/api.py
@@ -1,21 +1,12 @@
 """
 This is the user facing API of ocp_freecad_cam
 """
-import io
 import logging
-import tempfile
 from typing import Literal, Optional
 
-import FreeCAD
-import Part
 import Path.Log as Log
 import PathScripts.PathUtils as PathUtils
-from OCP.BRepTools import BRepTools
-from OCP.TopoDS import TopoDS_Builder, TopoDS_Compound, TopoDS_Face, TopoDS_Shape
 from Path.Dressup import DogboneII, Tags
-from Path.Main import Job as FCJob
-from Path.Post.Command import buildPostList
-from Path.Post.Processor import PostProcessor as FCPostProcessor
 from Path.Tool import Bit, Controller
 
 from ocp_freecad_cam.api_util import (
@@ -26,13 +17,9 @@ from ocp_freecad_cam.api_util import (
     extract_plane,
     extract_topods_shapes,
     map_params,
-    scale_shape,
     shape_source_to_compound,
-    shape_source_to_compound_brep,
-    shape_to_brep,
-    transform_shape,
 )
-from ocp_freecad_cam.common import FaceSource, Plane, PlaneSource, PostProcessor
+from ocp_freecad_cam.common import PlaneSource, PostProcessor
 from ocp_freecad_cam.fc_impl import (
     AdaptiveOp,
     DeburrOp,
@@ -47,7 +34,6 @@ from ocp_freecad_cam.fc_impl import (
     Surface3DOp,
     VCarveOp,
 )
-from ocp_freecad_cam.visualizer import visualize_fc_job
 
 try:
     import cadquery as cq
@@ -571,109 +557,6 @@ class Job:
         )
         self._add_op(op)
         return self
-
-
-# def extract_plane(plane_source: PlaneSource) -> Plane:
-#    if cq:
-#        # ace.transformShape(job.top.fG)
-#        if isinstance(plane_source, cq.Workplane):
-#            return plane_source.plane
-#
-#        elif isinstance(plane_source, cq.Plane):
-#            return plane_source
-#
-#        elif isinstance(plane_source, cq.Face):
-#            gp_pln = plane_source.toPln()
-#            ax3 = gp_pln.Position()
-#            origin = cq.Vector(ax3.Location())
-#            x_dir = cq.Vector(ax3.XDirection())
-#            normal = cq.Vector(ax3.Direction())
-#            return cq.Plane(origin, x_dir, normal)
-#
-#    if b3d:
-#        if isinstance(plane_source, b3d.Plane):
-#            return plane_source
-#        elif isinstance(plane_source, b3d.Face):
-#            return b3d.Plane(face=plane_source)
-#
-#    raise ValueError(f"Unknown type of plane: {type(plane_source)}")
-
-
-def forward_transform(plane: Plane, shape: TopoDS_Shape) -> TopoDS_Shape:
-    if cq and isinstance(plane, cq.Plane):
-        return cq.Shape(shape).transformShape(plane.fG).wrapped
-
-    elif b3d and isinstance(plane, b3d.Plane):
-        return plane.to_local_coords(b3d.Shape(shape)).wrapped
-
-    raise ValueError(f"Unknown type of plane: {type(plane)}")
-
-
-def reverse_transform(plane: Plane, shape: TopoDS_Shape) -> TopoDS_Shape:
-    if cq and isinstance(plane, cq.Plane):
-        return cq.Shape(shape).transformShape(plane.rG).wrapped
-
-    elif b3d and isinstance(plane, b3d.Plane):
-        return plane.from_local_coords(b3d.Shape(shape).wrapped)
-
-    raise ValueError(f"Unknown type of plane: {type(plane)}")
-
-
-def forward_transform_tsrf(plane: Plane):
-    if cq and isinstance(plane, cq.Plane):
-        return plane.fG.wrapped.Trsf()
-
-    elif b3d and isinstance(plane, b3d.Plane):
-        return plane.forward_transform.wrapped.Trsf()
-
-    raise ValueError(f"Unknown type of plane: {type(plane)}")
-
-
-def reverse_transform_tsrf(plane: Plane):
-    if cq and isinstance(plane, cq.Plane):
-        return plane.rG.wrapped.Trsf()
-
-    elif b3d and isinstance(plane, b3d.Plane):
-        return plane.reverse_transform.wrapped.Trsf()
-
-    raise ValueError(f"Unknown type of plane: {type(plane)}")
-
-
-def extract_faces(face_source: FaceSource) -> list[TopoDS_Face]:
-    if isinstance(face_source, list):
-        faces = []
-        for element in face_source:
-            faces += extract_faces(element)
-        return faces
-
-    if cq:
-        if isinstance(face_source, cq.Workplane):
-            return [f.wrapped for f in face_source.objects if isinstance(f, cq.Face)]
-        elif isinstance(face_source, cq.Face):
-            return [face_source.wrapped]
-    if b3d:
-        if isinstance(face_source, b3d.Face):
-            return [face_source.wrapped]
-
-    raise ValueError(f"Unknown type of face: {type(face_source)}")
-
-
-def shapes_to_compound(shapes: list[TopoDS_Shape]) -> TopoDS_Compound:
-    comp = TopoDS_Compound()
-    comp_builder = TopoDS_Builder()
-    comp_builder.MakeCompound(comp)
-
-    for s in shapes:
-        comp_builder.Add(comp, s)
-
-    return comp
-
-
-def to_brep(shape: TopoDS_Shape):
-    data = io.BytesIO()
-    BRepTools.Write_s(shape, data)
-    data.seek(0)
-    return data.read().decode("utf8")
 
 
 class Toolbit:
