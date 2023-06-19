c37fc288abf6c1b8d4f89a444d374aecec0431b4
Matti Eiden <snaipperi@gmail.com>
2023-06-19 14:56:02 +0300

    Create test_drill.py

---
+++ tests/test_drill.py
@@ -0,0 +1,23 @@
+import cadquery as cq
+
+from ocp_freecad_cam.api import Drill, Job
+
+def test_drill():
+    wp = (
+        cq.Workplane()
+        .box(5, 5, 2)
+        .faces(">Z")
+        .workplane()
+        .pushPoints([(-1.5, -1.5), (0, 1.5), (0.5, -1)])
+        .circle(0.5)
+        .cutThruAll()
+    )
+
+    top = wp.faces(">Z").workplane()
+    hole_edges = wp.faces("<Z").objects[0].innerWires()
+
+    tool = Drill(diameter="1 mm")
+    job = Job(top, wp)
+    job = job.drill(hole_edges, tool)
+    job.to_gcode()
+    job.show()
