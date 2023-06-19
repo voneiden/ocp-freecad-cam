3592c7952d9bb33478262a721619114b88ff67c5
Matti Eiden <snaipperi@gmail.com>
2023-06-19 13:16:08 +0300

    Add extra coverage for visualizer

--- tests/test_adaptive.py
+++ tests/test_adaptive.py
@@ -14,6 +14,6 @@ def test_cq_adaptive():
     lines = gcode.split("\n")
     start_index = lines.index("(Begin operation: AdaptiveOp_1)")
     finish_index = lines.index("(Finish operation: AdaptiveOp_1)")
-
+    job.show()  # coverage for visualizer
     # Don't care what it generates as long as it's at least 100 lines
     assert (finish_index - start_index) > 100
