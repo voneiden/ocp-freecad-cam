48938704f2e34912dc5c92c4625253bf33524733
Matti Eiden <snaipperi@gmail.com>
2023-06-21 18:25:20 +0300

    Add workaround for supporting FreeCAD CQ-Editor reload
    Ref #14

--- src/ocp_freecad_cam/__init__.py
+++ src/ocp_freecad_cam/__init__.py
@@ -1,4 +1,14 @@
-import FreeCAD  # noqa
+import sys
+
+# This needs to be done due to some weird bug that causes
+# ImportError: 'FreeCAD' is not a built-in module
+# For example in CQ-Editor if import reload is enabled
+if hasattr(sys, "_cached_freecad_module"):
+    sys.modules["FreeCAD"] = sys._cached_freecad_module
+else:
+    import FreeCAD  # noqa
+
+    sys._cached_freecad_module = sys.modules["FreeCAD"]
 
 from ocp_freecad_cam.api import Job
 from ocp_freecad_cam.api_tool import (
