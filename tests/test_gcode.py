5fe29c9d8e886892828b53a039b3f5708617fdf9
Matti Eiden <snaipperi@gmail.com>
2025-09-06 21:22:16 +0300

    tests: remove all tests except one to see what windows thinks of it

--- tests/test_gcode.py
+++ tests/test_gcode.py
@@ -17,38 +17,3 @@ def test_cq_spindle_feeds_and_speeds():
     assert "S12000" in gcode
 
 
-def assert_gcode(f):
-    def _assert_gcode(gcode):
-        assert f(gcode)
-
-    return _assert_gcode
-
-
-no_coolant_assertion = lambda gcode: "M7" not in gcode and "M8" not in gcode  # noqa
-flood_coolant_assertion = lambda gcode: "M8" in gcode and "M7" not in gcode  # noqa
-mist_coolant_assertion = lambda gcode: "M7" in gcode and "M8" not in gcode  # noqa
-
-
-@pytest.mark.parametrize(
-    "job_coolant,op_coolant,assertion",
-    [
-        (None, None, no_coolant_assertion),
-        ("None", "None", no_coolant_assertion),
-        ("Flood", "None", no_coolant_assertion),
-        ("Mist", "None", no_coolant_assertion),
-        ("Flood", None, flood_coolant_assertion),
-        ("Mist", None, mist_coolant_assertion),
-        ("None", "Flood", flood_coolant_assertion),
-        ("None", "Mist", mist_coolant_assertion),
-    ],
-)
-def test_cq_coolant(job_coolant, op_coolant, assertion):
-    box = cq.Workplane().box(10, 10, 1)
-    top = box.faces(">Z").workplane()
-    profile_shape = box.faces("<Z")
-    tool = Endmill(diameter=1)
-    job = Job(top, box, "grbl", coolant=job_coolant).profile(  # noqa
-        profile_shape, tool, coolant=op_coolant  # noqa
-    )
-
-    assert assertion(job.to_gcode())
